2b1d1397835b33d9900e799ae36ea98d719e2c5e
RhuanSantosIF <theo10ap@gmail.com>
2025-10-29 15:46:05 -0300

    correções

--- js.js
+++ js.js
@@ -1,6 +1,6 @@
 
 var nota;
-function addNota(){
+function addNota() {
     // pegar texto da nota
     nota = document.getElementById("Nota").value.trim();
 
@@ -8,16 +8,14 @@ function addNota(){
     var radioSelecionado = document.querySelector("input[name='urgencia']:checked");
 
     //colocar nota na div correta
-    if(radioSelecionado){
-        var divDestino = document.getElementById(radioSelecionado.value);
-        divDestino.innerHTML += `<p>${nota}</p>`;
-    }
-    else{
-        alert("Por favor, selecione a urgência da nota.");
-        return;
-    }
+    var divDestino = document.getElementById(radioSelecionado.value);
+
+    divDestino.innerHTML += `<il>${nota}</il>`;
+
 
     // limpar input e desmarcar radio
     document.getElementById('Nota').value = '';
     radioSelecionado.checked = false;
+
+    
 }
